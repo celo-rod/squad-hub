7dbf7bceac63dda85d526d47a40c9e16f2b96852
Lucas Bicalho <lucas.bicalho1@estudante.ufla.br>
2023-11-12 21:03:37 -0300

    :sparkles: feat: Criação da Tela Menu Administrador

--- front-trabalho/src/App.js
+++ front-trabalho/src/App.js
@@ -2,15 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/pages/Login';
 import MenuGerente from './components/pages/MenuGerente';
+import MenuAdministrador from './components/pages/MenuAdministrador';
 //tenho que fazer as rotas
 //olhar como linkar atraves dos botões div (resolução: linkar com icon e com o texto)
 //ajustar o tamanho dos quadradinhos no menu
 function App() {
   return (
     <div className="App">
-      
-    
-    <MenuGerente/>
+    <MenuAdministrador/>
     </div>
   );
 }
