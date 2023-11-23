cef1f02093db4cce8854c990110bb71c73c50c19
Lucas Bicalho <lucas.bicalho1@estudante.ufla.br>
2023-11-23 14:31:08 -0300

    :recycle: refactor: Alteração no formulario de cadastro de Projeto

--- front-trabalho/src/components/pages/Projeto/RegistroAndExcluirAndDadosProjeto.js
+++ front-trabalho/src/components/pages/Projeto/RegistroAndExcluirAndDadosProjeto.js
@@ -12,16 +12,14 @@ function REDProjeto({action, method, botao}) {
             <section className={style.primeiraJanela}>
 
             <form className={style.primeiraJanela1} action={action} method={method}>
-                    <input type="text" name='nomeProjeto' placeholder="Nome do Projeto" required></input>
-                    <input type="text" name='codigoProjeto' placeholder="Codigo" required></input>
-                    <input type="text" name='descricaoProjeto' placeholder="Descrição" required></input>
-            
-            </form>
-
-            <form className={style.segundaJanela} action={action} method="GET">
+                
+                <input type="text" name='nomeProjeto' placeholder="Nome do Projeto" required></input>
                 <input type="text" name='dataInicioProjeto' placeholder="Data Inicio" required></input>
+                <input type="text" name='codigoProjeto' placeholder="Codigo" required></input>
                 <input type="text" name='dataFimProjeto' placeholder="Data Término" required></input>
+                <input type="text" name='descricaoProjeto' placeholder="Descrição" required></input>
                 <input type="text" name='AtividadeProjeto' placeholder="Atividade" required></input>
+                <p></p>
                 <button type="submit"><Link to ="/projeto-cadastrado" style={{textDecoration: 'none', color: '#FFF'}}>{botao}</Link></button>
             </form>
 
@@ -41,17 +39,16 @@ function REDProjeto({action, method, botao}) {
 
             <section className={style.primeiraJanela}>
 
-            <form className={style.primeiraJanela1}>
-                    <input type="text" placeholder="Nome do Projeto"></input>
-                    <input type="text" placeholder="Codigo"></input>
-                    <input type="text" placeholder="Descrição"></input>
-            </form>
-
-            <form className={style.segundaJanela}>
-                <input type="text" placeholder="Data Inicio" ></input>
-                <input type="text" placeholder="Data Término" ></input>
-                <input type="text" placeholder="Atividade"></input>
-                <button><Link to ="/projeto-excluido" style={{textDecoration: 'none', color: '#FFF'}}>{botao}</Link></button>
+            <form className={style.primeiraJanela1} action={action} method={method}>
+                
+                <input type="text" name='nomeProjeto' placeholder="Nome do Projeto" required></input>
+                <input type="text" name='dataInicioProjeto' placeholder="Data Inicio" required></input>
+                <input type="text" name='codigoProjeto' placeholder="Codigo" required></input>
+                <input type="text" name='dataFimProjeto' placeholder="Data Término" required></input>
+                <input type="text" name='descricaoProjeto' placeholder="Descrição" required></input>
+                <input type="text" name='AtividadeProjeto' placeholder="Atividade" required></input>
+                <p></p>
+                <button type="submit"><Link to ="/projeto-cadastrado" style={{textDecoration: 'none', color: '#FFF'}}>{botao}</Link></button>
             </form>
 
             </section>
@@ -70,17 +67,16 @@ function REDProjeto({action, method, botao}) {
 
             <section className={style.primeiraJanela}>
 
-            <form className={style.primeiraJanela1}>
-                    <input type="text" placeholder="Nome do Projeto"></input>
-                    <input type="text" placeholder="Codigo"></input>
-                    <input type="text" placeholder="Descrição"></input>
-            </form>
-
-            <form className={style.segundaJanela}>
-                <input type="text" placeholder="Data Inicio" ></input>
-                <input type="text" placeholder="Data Término" ></input>
-                <input type="text" placeholder="Atividade"></input>
-                <button><Link to ="/gerenciar-projeto" style={{textDecoration: 'none', color: '#FFF'}}>{botao}</Link></button>
+            <form className={style.primeiraJanela1} action={action} method={method}>
+                
+                <input type="text" name='nomeProjeto' placeholder="Nome do Projeto" required></input>
+                <input type="text" name='dataInicioProjeto' placeholder="Data Inicio" required></input>
+                <input type="text" name='codigoProjeto' placeholder="Codigo" required></input>
+                <input type="text" name='dataFimProjeto' placeholder="Data Término" required></input>
+                <input type="text" name='descricaoProjeto' placeholder="Descrição" required></input>
+                <input type="text" name='AtividadeProjeto' placeholder="Atividade" required></input>
+                <p></p>
+                <button type="submit"><Link to ="/projeto-cadastrado" style={{textDecoration: 'none', color: '#FFF'}}>{botao}</Link></button>
             </form>
 
             </section>
